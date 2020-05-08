523c10d0e91714176fc2cdd69d8a8b2102cde808
borhenenib@gmail.com <BorhenCherif>
2020-05-09 00:35:38 +0100

    first commit

---
+++ ConsoleApp1/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class Calculator
+    {
+       public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double result;
+
+            switch(argOperation.ToLower())
+            {
+
+                case "add":
+                case "+":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+                case "substract":
+                case "-":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+                case "multiplay":
+                case "*":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                case "divide":
+                case "/":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+                default:
+
+                 throw new InvalidOperationException("specify operation not regonise");
+
+            }
+
+            return result;
+
+        }
+             
+            
+
+        }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Input input = new Input();
+                Calculator calculator = new Calculator();
+                double firstNumber = input.ConvertInputToNumeric(Console.ReadLine());
+                double secondNumber = input.ConvertInputToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
+                double result = calculator.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
